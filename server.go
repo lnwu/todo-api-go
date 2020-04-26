92c6dc5291f2d53e8514af9abc476db49551cb96
lnwu <wind2729@gmail.com>
2020-04-26 14:35:40 +0800

    add cors config

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/lnwu/todo-api-go/src/config"
 	"github.com/lnwu/todo-api-go/src/routes"
@@ -12,6 +13,10 @@ func main() {
 	config.Connect()
 
 	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000/", "http://todo-web-development.s3-ap-northeast-1.amazonaws.com/", "http://todo-web-staging.s3-ap-northeast-1.amazonaws.com/"}
+	router.Use(cors.New(config))
 
 	routes.Routes(router)
 
