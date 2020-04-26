600cee1a9c2df8151b7f08b4516e83e588a42900
lnwu <wind2729@gmail.com>
2020-04-26 14:40:54 +0800

    allow all origins

--- server.go
+++ server.go
@@ -14,9 +14,7 @@ func main() {
 
 	router := gin.Default()
 
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000/", "http://todo-web-development.s3-ap-northeast-1.amazonaws.com/", "http://todo-web-staging.s3-ap-northeast-1.amazonaws.com/"}
-	router.Use(cors.New(config))
+	router.Use(cors.Default())
 
 	routes.Routes(router)
 
