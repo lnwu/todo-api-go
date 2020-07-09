a5dea5079fe72a48593e21e6b5ac891ea13374f7
lnwu <wind2729@gmail.com>
2020-07-09 23:37:27 +0800

    test ci build

--- auto/build.sh
+++ auto/build.sh
@@ -2,4 +2,10 @@
 
 set -euo pipefail
 
-docker build . -t lnwu/todo-api
+TAG=ccr.ccs.tencentyun.com/k8-test/todo-api:${CI_BUILD_NUMBER}
+
+docker build . -t ${TAG}
+
+docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} ccr.ccs.tencentyun.com
+docker push ${TAG}
+docker logout ccr.ccs.tencentyun.com
